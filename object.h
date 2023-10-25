9b73ac2cb487cf85be0e8b8d39f777b1b53fa644
Bentalha Aymen <>
2023-10-25 09:34:25 +0200

    recuperation #2 des fichier player feature4 vers feature2

---
+++ object.h
@@ -0,0 +1,22 @@
+/*#ifndef OBJECT_H
+#define OBJECT_H
+
+typedef struct Object
+{
+    int id;
+    int durability;
+    int quantity;
+} Object;
+
+Object initObject(int,int,int);
+Object blankObject();
+int stackable(Object*,Object*);
+int isWeapon(int);
+int isCraft(int);
+int isArmor(int);
+Object sword(int);
+Object plant(int,int);
+Object lance(int);
+void resetObject(Object*);
+
+#endif  //OBJECT_H*/
